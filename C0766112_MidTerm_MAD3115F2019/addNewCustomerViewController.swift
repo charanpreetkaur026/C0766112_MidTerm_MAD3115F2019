29a2209dfeb20806302f820cee82082dd20f0b76
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-11-01 17:21:09 -0400

    [charan]  add customer

--- C0766112_MidTerm_MAD3115F2019/addNewCustomerViewController.swift
+++ C0766112_MidTerm_MAD3115F2019/addNewCustomerViewController.swift
@@ -38,5 +38,35 @@ class addNewCustomerViewController: UIViewController {
             navigationController?.pushViewController(customerListVC, animated: true)
     
 
-}
+    
+    let firstName = txtFirstName.text
+    let lastName = txtLastName.text
+            func  isValidEmail() -> Bool
+            {
+                let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+                let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+                return emailTest.evaluate(with: self)
+            }
+            if isValidEmail()
+            {
+                
+                let email = txtEmail.text
+                obj.addNewCustomer(First_Name: firstName!, Last_Name: lastName!, email: email!)
+                
+                let alert = UIAlertController(title: "Success", message: "Congrats! Added Successfully", preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                
+                self.present(alert, animated: true)
+            }
+            else{
+                
+                let alert = UIAlertController(title: "INVALID EMAIL!", message: "Please Enter a Valid Email Address", preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                
+                self.present(alert, animated: true)
+                
+            }
+    }
 }
